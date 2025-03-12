35bb35b849834eedec49df01cc363959e01f0fb7
Elias Gabriel Amaral da Silva <tolkiendili@gmail.com>
2025-03-12 08:47:37 -0300

    well this works

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,29 +1,12 @@
-use inotify::{Inotify, WatchMask};
+//use inotify::{Inotify, WatchMask};
 use std::error::Error;
+use waitpid_any::WaitHandle;
 
 fn wip(pid: i32) -> Result<(), Box<dyn Error>> {
-    println!("Monitoring process with PID: {}", pid);
-
-    // Check if process exists initially
-    let proc_path = format!("/proc/{}", pid);
-    if !std::path::Path::new(&proc_path).exists() {
-        return Err(format!("Process {} does not exist", pid).into());
-    }
-
-    // Initialize inotify
-    let mut inotify = Inotify::init()?;
-
-    // Watch for DELETE_SELF event on the process directory
-    inotify.watches().add(&proc_path, WatchMask::DELETE_SELF)?;
-
-    // Buffer for events
-    let mut buffer = [0; 1024];
-
-    // Wait for events
-    println!("Waiting for process {} to terminate...", pid);
-    let _ = inotify.read_events_blocking(&mut buffer)?;
-
-    println!("Process {} has terminated", pid);
+    let mut wait_handle = WaitHandle::open(pid)?;
+    println!("Waiting for process {} to exit...", pid);
+    wait_handle.wait()?;
+    println!("Process {} exited", pid);
     Ok(())
 }
 
